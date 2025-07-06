f5cf6c2c5f411acd3b01ea878dbe9b722324ad70
tokkaiiii <luv_3_u@naver.com>
2025-07-06 12:42:42 +0900

    feat: 속성 추가

--- src/main/java/com/clean/hexagonal/domain/RamenShop.java
+++ src/main/java/com/clean/hexagonal/domain/RamenShop.java
@@ -1,5 +1,13 @@
 package com.clean.hexagonal.domain;
 
+import java.time.LocalDateTime;
+
 public class RamenShop {
+
+  private String name;
+  private String address;
+  private String status;
+  private LocalDateTime openingTime;
+  private LocalDateTime closingTime;
 
 }
