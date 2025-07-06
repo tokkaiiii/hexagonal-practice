4b3f7532a0ea437039549be0d2cb5a51047f5e39
tokkaiiii <luv_3_u@naver.com>
2025-07-06 12:51:25 +0900

    feat: 도메인 행위 메소드 register 추가

--- src/main/java/com/clean/hexagonal/domain/RamenShop.java
+++ src/main/java/com/clean/hexagonal/domain/RamenShop.java
@@ -1,13 +1,31 @@
 package com.clean.hexagonal.domain;
 
-import java.time.LocalDateTime;
+import static com.clean.hexagonal.domain.RamenShopStatus.OPEN;
+
+import java.time.LocalTime;
 
 public class RamenShop {
 
   private String name;
   private String address;
-  private String status;
-  private LocalDateTime openingTime;
-  private LocalDateTime closingTime;
+  private RamenShopStatus status;
+  private LocalTime openingTime;
+  private LocalTime closingTime;
 
+  public static RamenShop register(
+      String name,
+      String address,
+      LocalTime openingTime,
+      LocalTime closingTime
+  ) {
+    RamenShop ramenShop = new RamenShop();
+    ramenShop.name = name;
+    ramenShop.address = address;
+    ramenShop.openingTime = openingTime;
+    ramenShop.closingTime = closingTime;
+
+    ramenShop.status = OPEN;
+
+    return ramenShop;
+  }
 }
